922b59f9938522eb80ce653ed4d2dc4445c76bf6
Luiz Porto <piilamster@gmail.com>
2022-10-15 11:05:19 -0300

    add services, services-modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,27 @@ const tabs = document.querySelectorAll('[data-target'),
         })
       })
 // ------------------ SERVICES MODAL -------------
+const modalViews = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalCloses = document.querySelectorAll('.services_modal-close')
 
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+} 
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})  
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 // --------------------- PORTFOLIO SWIPER ----------------
 
 // --------------------- TESTIMONIAL -------------------
